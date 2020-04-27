dbf727f73909318818e08c6eb5130f1c666f6a1c
auliafitriyani <fitriyaniaulia2@gmail.com>
2020-04-27 14:40:33 +0700

    first commit

--- cobaapp/urls.py
+++ cobaapp/urls.py
@@ -7,4 +7,6 @@ app_name = 'cobaapp'
 urlpatterns=[
     url(r'^index/$',views.index,name='index'),
     url(r'^dua/$',views.dua,name='dua'),
+    url(r'^format/$',views.format,name='format'),
+    url(r'^contact/$',views.contact,name='contact'),
 ]
--- cobaapp/views.py
+++ cobaapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-
+from cobaapp.models import Employee
+from django.http import HttpResponse
+from .forms import ContactForm
 # Create your views here.
 def index(request):
     return render(request,'cobaapp/index.html')
 
 def dua(request):
-    return render(request,'cobaapp/dua.html')
-
-def people(request):
     istekler = Employee.objects.all()
-    return render(request, 'dua.html', locals())
+    context = {'istekler':istekler}
+    return render(request,'cobaapp/dua.html',context)
+
+def format(request):
+    return render(request,'cobaapp/format.html')
+
+def contact(request):
+    form = ContactForm()
+    return render(request, 'cobaapp/format.html', {'form':form})
